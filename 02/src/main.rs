abcf00a828c2dd7de71974bc429e18d9c4bd742a
Drew Stokes <drew@drewstokes.com>
2022-12-02 08:18:48 -0800

    add game fn

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -2,11 +2,28 @@ use std::fs::File;
 use std::collections::HashMap;
 use std::io::{self, prelude::*, BufReader};
 
+const ROCK: u8 = 1;
+const PAPER: u8 = 2;
+const SCISSOR: u8 = 3;
+
+fn hand_wins(moves: Split<&str>) -> bool {
+    match hand_one {
+        ROCK=>hand_two==SCISSOR,
+        PAPER=>hand_two==ROCK,
+        SCISSOR=>hand_two==PAPER,
+    }
+}
+
 fn main() -> io::Result<()> {
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
 
     for line in reader.lines() {
+        println(
+            hand_wins(
+                line.split_whitespace()
+            )
+        )
     }
 
     println!("Score: ");
