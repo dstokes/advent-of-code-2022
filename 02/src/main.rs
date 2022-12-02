93529a92d01eae2bd3a143d8f4b8d3847616d457
Drew Stokes <drew@drewstokes.com>
2022-12-02 08:56:21 -0800

    wip

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -1,12 +1,16 @@
 use std::fs::File;
 use std::collections::HashMap;
+use std::str::Split;
 use std::io::{self, prelude::*, BufReader};
 
 const ROCK: u8 = 1;
 const PAPER: u8 = 2;
 const SCISSOR: u8 = 3;
 
-fn hand_wins(moves: Split<&str>) -> bool {
+fn str_to_move(
+
+fn hand_wins(moves: (&str, &str)) -> bool {
+    let (hand_one, hand_two) = moves;
     match hand_one {
         ROCK=>hand_two==SCISSOR,
         PAPER=>hand_two==ROCK,
@@ -19,11 +23,7 @@ fn main() -> io::Result<()> {
     let reader = BufReader::new(file);
 
     for line in reader.lines() {
-        println(
-            hand_wins(
-                line.split_whitespace()
-            )
-        )
+        println!("{}", hand_wins(line.unwrap().split_once(" ").unwrap()))
     }
 
     println!("Score: ");
