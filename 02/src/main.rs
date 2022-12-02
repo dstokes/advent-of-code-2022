ba7eccdb60a1b5e6eeefaac21b467932980177b1
Drew Stokes <drew@drewstokes.com>
2022-12-02 12:01:56 -0800

    part 1

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -1,31 +1,65 @@
 use std::fs::File;
-use std::collections::HashMap;
-use std::str::Split;
 use std::io::{self, prelude::*, BufReader};
 
 const ROCK: u8 = 1;
 const PAPER: u8 = 2;
 const SCISSOR: u8 = 3;
 
-fn str_to_move(
+const WIN: u8 = 6;
+const DRAW: u8 = 3;
+const LOSS: u8 = 0;
 
-fn hand_wins(moves: (&str, &str)) -> bool {
-    let (hand_one, hand_two) = moves;
-    match hand_one {
-        ROCK=>hand_two==SCISSOR,
-        PAPER=>hand_two==ROCK,
-        SCISSOR=>hand_two==PAPER,
+fn to_moves(codes: (&str, &str)) -> (u8, u8) {
+    (to_move(codes.0), to_move(codes.1))
+}
+
+fn to_move(code: &str) -> u8 {
+    return match code {
+        "A" | "X" => ROCK,
+        "B" | "Y" => PAPER,
+        "C" | "Z" => SCISSOR,
+        _ => ROCK
+    }
+}
+
+fn to_scores(moves: (u8, u8)) -> (u8, u8) {
+    if moves.0== moves.1 {
+        return (DRAW, DRAW)
+    }
+
+    match moves.0 {
+        ROCK=>
+            match moves.1 {
+                SCISSOR=>(WIN, LOSS),
+                PAPER=>(LOSS, WIN),
+                _=>(LOSS, LOSS)
+            }
+        PAPER=>
+            match moves.1 {
+                ROCK=>(WIN, LOSS),
+                SCISSOR=>(LOSS, WIN),
+                _=>(LOSS, LOSS)
+            }
+        SCISSOR=>
+            match moves.1 {
+                PAPER=>(WIN, LOSS),
+                ROCK=>(LOSS, WIN),
+                _=>(LOSS, LOSS)
+            }
+        _ => (DRAW,DRAW)
     }
 }
 
 fn main() -> io::Result<()> {
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
+    let mut score: i16 = 0;
 
     for line in reader.lines() {
-        println!("{}", hand_wins(line.unwrap().split_once(" ").unwrap()))
+        let moves = to_moves( line.unwrap().split_once(" ").unwrap());
+        score += to_scores(moves).1 as i16 + moves.1 as i16;
     }
 
-    println!("Score: ");
+    println!("Score: {}", score);
     Ok(())
 }
