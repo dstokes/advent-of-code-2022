ca4b5138ff272684837c9486f84bed436b39b9a8
Drew Stokes <drew@drewstokes.com>
2022-12-03 22:27:18 -0800

    day 3, part 2

--- 04/src/main.rs
+++ 04/src/main.rs
@@ -9,26 +9,33 @@ fn main() {
     let input =
         fs::read_to_string("input.txt").unwrap();
 
-    // part 1
-    let mut matches = 0;
+    let mut part1 = 0;
+    let mut part2 = 0;
     for line in input.lines() {
         // [start, end, start, end]
         let chars: Vec<&str> = line.split(&[',','-']).collect();
         let mut range1: HashSet<u32> = HashSet::new();
         let mut range2: HashSet<u32> = HashSet::new();
 
-        // [start ... end]
+        // create range [start ... end]
         for i in to_i(chars[0])..=to_i(chars[1]) {
             range1.insert(i);
         }
-        // [start ... end]
+        // create range [start ... end]
         for i in to_i(chars[2])..=to_i(chars[3]) {
             range2.insert(i);
         }
 
+        // part 1
         if range1.is_subset(&range2) || range2.is_subset(&range1) {
-            matches += 1
+            part1 += 1
+        }
+
+        // part 2
+        if range1.is_disjoint(&range2) == false {
+            part2 += 1;
         }
     }
-    println!("Part 1: {}", matches) // 471
+    println!("Part 1: {}", part1);  // 471
+    println!("Part 2: {}", part2); // 888
 }
