a81ac4c7216374f0c7e455189166caecad40eba3
Drew Stokes <drew@drewstokes.com>
2022-12-04 09:16:03 -0800

    simplify hashset creation

--- 04/src/main.rs
+++ 04/src/main.rs
@@ -14,17 +14,12 @@ fn main() {
     for line in input.lines() {
         // [start, end, start, end]
         let chars: Vec<&str> = line.split(&[',','-']).collect();
-        let mut range1: HashSet<u32> = HashSet::new();
-        let mut range2: HashSet<u32> = HashSet::new();
 
-        // create range [start ... end]
-        for i in to_i(chars[0])..=to_i(chars[1]) {
-            range1.insert(i);
-        }
-        // create range [start ... end]
-        for i in to_i(chars[2])..=to_i(chars[3]) {
-            range2.insert(i);
-        }
+        // create hashsets from ranges [start ... end]
+        let range1: HashSet<u32> = HashSet::from_iter(
+            to_i(chars[0])..=to_i(chars[1]));
+        let range2: HashSet<u32> = HashSet::from_iter(
+            to_i(chars[2])..=to_i(chars[3]));
 
         // part 1
         if range1.is_subset(&range2) || range2.is_subset(&range1) {
